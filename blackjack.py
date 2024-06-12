0f9b9c298a14b4d47c26eb17d12ad597b5c7d9e3
Atul Venkatesan <atulvenky@gmail.com>
2024-06-12 12:34:55 +0530

    added intro screen

--- blackjack.py
+++ blackjack.py
@@ -4,3 +4,28 @@ from tkinter import ttk
 root = tk.Tk()
 root.title("Black Jack 2.0")
 root.configure(background='wheat3')
+
+
+def save_name():
+    name = name_entry.get()
+    name_field.delete(0, tk.END)
+    greeting = f"Hello {name}"
+    name_field.insert(0, greeting)
+    # initialize the play_game() function here to clear the screen and start the game
+
+
+name_label = tk.Label(root, fg="navy", text="Enter your name")
+name_entry = tk.Entry(root, bg="MistyRose2")
+name_button = tk.Button(root, font=('arial', 15, 'bold'), text="Enter",  bg="magenta4", fg="magenta4", command= save_name)
+name_field = tk.Entry(root, bg="SeaGreen1")
+
+
+
+
+name_label.pack(padx=5, pady=5, side=tk.LEFT)
+name_entry.pack(padx=5, pady=5, side=tk.LEFT)
+name_button.pack(padx=5, pady=5, side=tk.LEFT)
+name_field.pack(padx=5, pady=5, side=tk.LEFT)
+
+
+root.mainloop()
